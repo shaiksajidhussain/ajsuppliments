20f605604421a927c5d07b72a19b1fd39b0a901b
shaiksajidhussain <sanjusazid0@gmail.com>
2025-09-18 10:28:44 +0530

    Added the our software page

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
